00194bd327a16d513b4d433177a77e20c825d139
zhxu7000 <zhenkexu763@gmail.com>
2023-08-08 18:17:45 +1000

    init project

---
+++ src/main/java/com/usyd/emergency/EmergencyApplication.java
@@ -0,0 +1,13 @@
+package com.usyd.emergency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmergencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmergencyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/usyd/emergency/EmergencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usyd.emergency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmergencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
