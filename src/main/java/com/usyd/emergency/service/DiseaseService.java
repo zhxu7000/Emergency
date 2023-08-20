1b85339472cee981698b5efcfebe9bfa8a0d5c13
zhenkexu <zhxu7000@uni.sydney.edu.au>
2023-08-20 13:20:23 +1000

    updata disease

---
+++ src/main/java/com/usyd/emergency/service/DiseaseService.java
@@ -0,0 +1,34 @@
+package com.usyd.emergency.service;
+
+import com.usyd.emergency.pojo.Disease;
+import com.usyd.emergency.repository.DiseaseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityNotFoundException;
+
+@Service
+public class DiseaseService {
+
+    private final DiseaseRepository diseaseRepository;
+
+    @Autowired
+    public DiseaseService(DiseaseRepository diseaseRepository) {
+        this.diseaseRepository = diseaseRepository;
+    }
+
+    public Disease findDiseaseByName(String diseaseName) {
+        return diseaseRepository.findByDiseaseName(diseaseName);
+    }
+
+    public Disease saveDisease(Disease disease) {
+        return diseaseRepository.save(disease);
+    }
+
+    public void deleteDiseaseById(int diseaseId) {
+        if (!diseaseRepository.existsById(diseaseId)) {
+            throw new EntityNotFoundException("Disease not found with ID: " + diseaseId);
+        }
+        diseaseRepository.deleteById(diseaseId);
+    }
+}
