e31da919cd80855d3c1a3d6eef20a17fb9682fd2
zhenkexu <zhxu7000@uni.sydney.edu.au>
2023-08-22 23:12:35 +1000

    modify diseaseController

--- src/main/java/com/usyd/emergency/controller/DiseaseController.java
+++ src/main/java/com/usyd/emergency/controller/DiseaseController.java
@@ -1,17 +1,31 @@
 package com.usyd.emergency.controller;
 
+import com.usyd.emergency.pojo.Disease;
 import com.usyd.emergency.service.DiseaseService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 public class DiseaseController {
-
     @Autowired
     DiseaseService diseaseService;
+    public DiseaseController(DiseaseService diseaseService) {
+        this.diseaseService = diseaseService;
+    }
 
-    @PostMapping("/")
-    public String
+    @GetMapping("/{diseaseName}")
+    public Disease getDiseaseByName(@PathVariable String diseaseName) {
+        return diseaseService.findDiseaseByName(diseaseName);
+    }
 
+    @PostMapping
+    public Disease createDisease(@RequestBody Disease disease) {
+        return diseaseService.saveDisease(disease);
+    }
+
+    @DeleteMapping("/{diseaseId}")
+    public void deleteDisease(@PathVariable int diseaseId) {
+        diseaseService.deleteDiseaseById(diseaseId);
+    }
 }
